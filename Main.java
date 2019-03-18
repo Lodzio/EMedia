3919dbab3ad7e98840bdcb2f071cce0a63e14c25
Bartek <lodzio1997@wp.pl>
2019-03-18 13:03:35 +0100

    add pictrue file and read bytes from file

--- Main.java
+++ Main.java
@@ -1,7 +1,27 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String fileName = "Kwiatek_1533.jpg";
+        int buffer;
+        System.out.println(fileName);
+        try{
+            FileInputStream fileReader = new FileInputStream(fileName);
+            while((buffer = fileReader.read()) != -1) {
+                System.out.println(Integer.toHexString(buffer));
+            } 
+            System.out.println(buffer);
+            fileReader.close();
+        } catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                fileName + "'");                
+        } catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + fileName + "'"); 
+        }
     }
 
 }
